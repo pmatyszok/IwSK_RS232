5fdc0d3132cae862c68c8b9d6594c56be138bc9b
tomasz-baron <tomek91b@gmail.com>
2014-05-07 19:30:17 +0200

    Funkcje konwertujące string na byte i byte na string

--- src/Modbus/ModbusClass.cs
+++ src/Modbus/ModbusClass.cs
@@ -41,6 +41,61 @@ namespace IwSK_RS232.Modbus
             return (byte)(~sum+(byte)1);
         }
 
+        private byte stringToByte(string data)
+        {
+            byte result = 0;
+            int i, temp = 0;
+            for(i = 0; i < data.Length; i++)
+            {
+                if (data[i] >= 'A' && data[i] <= 'F')
+                {
+                    temp = Convert.ToInt16(data[i]) - 55;
+                }
+                if (data[i] >= '0' && data[i] <= '9')
+                {
+                    temp = Convert.ToInt16(data[i]) - 48;
+                }
+                if (i == 0)
+                {
+                    result += Convert.ToByte(temp << 4);
+                } 
+                else if (i == 1)
+                {
+                    result += Convert.ToByte(temp);
+                }
+            }
+                return result;
+        }
+
+        private string byteToString(byte data)
+        {
+            string result = " ";
+            char tmp;
+            int temp = data & 0x0F;
+            if (temp >= 10 && temp <= 15)
+            {
+                tmp = Convert.ToChar(temp + 55);
+            }
+            if (temp >= 0 && temp <= 9)
+            {
+                tmp = Convert.ToChar(temp + 48);
+            }
+
+
+            temp = data & 0xF0;
+            temp = temp >> 4;
+            if (temp >= 10 && temp <= 15)
+            {
+                tmp = Convert.ToChar(temp + 55);
+            }
+            if (temp >= 0 && temp <= 9)
+            {
+                tmp = Convert.ToChar(temp + 48);
+            }
+
+            return result;
+        }
+
  
 
     }
